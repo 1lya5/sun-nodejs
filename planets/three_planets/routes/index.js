2d210d7cd9ecc292953b88dbe8a1d4e3834777e1
1lya5 <ilyas.mustafayev.05@mail.ru>
2024-01-14 21:52:17 +0300

    маршрутизатор

--- planets/three_planets/routes/index.js
+++ planets/three_planets/routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* Страница Марса */
+router.get('/mars', function(req, res, next) {
+  res.send("<h1>Страница Марс</h1>")
+});
+/* Страница Земли */
+router.get('/earth', function(req, res, next) {
+  res.send("<h1>Страница Земля</h1>")
+});
+/* Страница Венеры */
+router.get('/venera', function(req, res, next) {
+  res.send("<h1>Страница Венера</h1>")
+});
+
 module.exports = router;
